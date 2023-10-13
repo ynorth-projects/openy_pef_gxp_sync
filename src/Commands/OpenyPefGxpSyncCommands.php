f62d1ef83c75348947c44c3059d22263fcf0a0fb
svicervlad <vlad.sadretdinov@jet.dev>
2023-10-13 11:00:58 +0000

    feat: upgrade module to drupal 10

--- src/Commands/OpenyPefGxpSyncCommands.php
+++ src/Commands/OpenyPefGxpSyncCommands.php
@@ -2,31 +2,12 @@
 
 namespace Drupal\openy_pef_gxp_sync\Commands;
 
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drush\Commands\DrushCommands;
 
 /**
  * Groupex sync drush commands.
  */
 class OpenyPefGxpSyncCommands extends DrushCommands {
-
-  /**
-   * The logger channel factory.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
-   */
-  protected $logger;
-
-  /**
-   * Constructs a new OpenyPefGxpSyncCommands.
-   *
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
-   *   The logger channel factory.
-   */
-  public function __construct(LoggerChannelFactoryInterface $logger) {
-    parent::__construct();
-    $this->logger = $logger;
-  }
 
   /**
    * Run syncer.
@@ -39,7 +20,7 @@ class OpenyPefGxpSyncCommands extends DrushCommands {
       ymca_sync_run("openy_pef_gxp_sync.syncer", "proceed");
     }
     catch (\Exception $e) {
-      $this->logger->get('openy_pef_gxp_sync')
+      $this->logger()
         ->error('Failed to run syncer with message: %message', ['%message' => $e->getMessage()]);
     }
   }
